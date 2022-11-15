4e6d98adfaab9026fc3e808b149df05315515ff0
KaipoCraft <71410871+KaipoCraft@users.noreply.github.com>
2022-11-15 14:48:49 -0700

    AI Edits

--- unity/final_project/Assets/Scripts/AI/AI.cs
+++ unity/final_project/Assets/Scripts/AI/AI.cs
@@ -70,20 +70,85 @@ public class AI
             // for now, set up to move at random, further playtesting will reveal if this is good enough
             // reasoning: units will not immediately go far away from the capital city and will likely spread around in the area
 
-            // scan every neighboring cell
-            foreach (Cell n in neighbors)
+           // BFS to find the nearest enemy city
+           // Get coordinates of the city, getCell().getCoordinates() 
+           // Look at X and Z of the unit and the city
+           // Between the possible moves, look for move that would get theirs and the city's coordinates closer
+
+           // get location of the civilization's city
+            Cell startingCell = unit.getCell();
+            List<Cell> cells = new List<Cell>();
+            cells.Add(startingCell);
+            Unit choice = null;
+
+            List<Cell> notConsidered = new List<Cell>();
+
+            while (cells.Count != 0)
             {
-                // check if neighboring cell has a unit from another civ
-                if (n.isFree())
+                List<Cell> newList = new List<Cell>();
+                // scan every cell in the list
+                foreach (Cell cell_ in cells)
                 {
-                    // 70% chance to move into the cell
-                    if (Random.value < 0.9f)
+                    notConsidered.Add(cell_);
+
+                    List<Cell> neighbors2 = cell_.getAdjacent();
+
+                    // scan every neighbor of a cell from the list
+                    foreach (Cell n in neighbors2)
                     {
-                        unit.move(n);
-                        break;
+                        if (n.isFree() || n.getUnit() is not UnitCity || (n.getUnit() is UnitCity && n.getUnit().getCiv() == unit.getCiv()))
+                        {
+                            // if neighbor isn't free and hasn't already been traversed, it'll be in the new traversal list
+                            
+                            if (!notConsidered.Contains(n) && !newList.Contains(n))
+                                newList.Add(n);
+                        }
+                        else
+                        {
+                            // if neighbor is free, we pick it and terminate
+                            choice = n.getUnit();
+                            break;
+                        }
                     }
+
+                    // check if a cell has been selected for spawn
+                    if (choice != null)
+                        break;
+
+                }
+
+                if (choice != null)
+                    break;
+
+                cells = newList;
+            }
+            // now we know what the closest enemy city is
+            // get coordinates for the city
+            int[] cityCoords = choice.getCell().getCoordinates();
+            int[] unitCoords = unit.getCell().getCoordinates();
+
+            Cell destination = null;
+            foreach (Cell n in unit.getCell().getAdjacent()) {
+                // look through the cells and find which one will get it closer to the 
+                if ((cityCoords[0] > unitCoords[0]) && (n.getCoordinates()[0] > unitCoords[0]) ) {
+                    destination = n;
+                    break;
+                } else if ((cityCoords[2] > unitCoords[2]) && (n.getCoordinates()[2] > unitCoords[2])) {
+                    destination = n;
+                    break;
+                }
+                if ((cityCoords[0] < unitCoords[0]) && (n.getCoordinates()[0] < unitCoords[0]) ) {
+                    destination = n;
+                    break;
+                } else if ((cityCoords[2] < unitCoords[2]) && (n.getCoordinates()[2] < unitCoords[2])) {
+                    destination = n;
+                    break;
                 }
             }
+            if (destination != null) {
+                unit.move(destination);
+            }
+        
         }
     }
 }
--- unity/final_project/Assets/Scripts/Game.cs
+++ unity/final_project/Assets/Scripts/Game.cs
@@ -48,7 +48,7 @@ public class Game : MonoBehaviour
             }
 
             // wait for 1 second
-            yield return new WaitForSeconds(0.1f);
+            yield return new WaitForSeconds(1f);
         }
     }
 
